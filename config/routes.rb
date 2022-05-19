2bbd816067e9fcc29ac82ae0dc06b9919087086f
Uzor Efedigbue <cefedigbuej@gmail.com>
2022-05-19 14:11:12 +0100

    setup blog

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  get 'about', to: 'pages#about'
+  # get 'home', to: 'pages#home'
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root 'pages#home'
+end
