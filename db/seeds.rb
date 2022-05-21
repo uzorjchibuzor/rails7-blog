dc1b7d6c22d5c533a69a9c9087474fea069dba57
Uzor Efedigbue <cefedigbuej@gmail.com>
2022-05-21 19:08:15 +0100

    adding posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+15.times do |i|
+    Post.create(title: "Title #{i}", body: "#{i} Words go here")
+end
